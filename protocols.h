62ba65b5b33c2a5058eef625970e5928cd013ab7
ddzibela <22411163+ddzibela@users.noreply.github.com>
2021-04-25 12:33:22 +0200

    add file

---
+++ protocols.h
@@ -0,0 +1,11 @@
+/**
+ * @author Denis Dzibela xdzibe00
+ * @brief A short list of IP protocol numbers found in the field Protocol of the IPv4 header 
+ * and the Next Header field of the IPv6 header.
+ * https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+ */
+#define prot_ICMP   0x01
+#define prot_TCP    0x06
+#define prot_UDP    0x11
+#define prot_IPv6_ICMP  0x3A
+#define prot_IPv6_NoNxt 0x3B
